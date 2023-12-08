f05e72e83752e3b6a4a6d102d7d47579f8f8622f
Vineeth Reddy <122296871+vineeth-reddy28@users.noreply.github.com>
2023-12-08 12:41:39 +0530

    mylatestcc

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,7 +35,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
-    System.out.println("Hello, World");
     System.out.println("Hello, Devops");
     System.out.println("Hello, JENKINS");
     System.out.println("WELCOME TO JENKINS SESSION");
